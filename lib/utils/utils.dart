01b64a61a661b8c774a3211aa562ee15db483f04
Phatsakorn Chosungnoen <phatsakorn.newz@gmail.com>
2022-10-07 11:55:20 +0700

    [ADD] Starter app done

---
+++ lib/utils/utils.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// Define App ID and Token
+const appID = "6655110553b7404a8bbd759a0f868785";
+const toKen = "";
+
+MaterialColor primaryColor = MaterialColor(0xfff160C2F, color);
+
+Map<int, Color> color = {
+  50: const Color.fromRGBO(136, 14, 79, .1),
+  100: const Color.fromRGBO(136, 14, 79, .2),
+  200: const Color.fromRGBO(136, 14, 79, .3),
+  300: const Color.fromRGBO(136, 14, 79, .4),
+  400: const Color.fromRGBO(136, 14, 79, .5),
+  500: const Color.fromRGBO(136, 14, 79, .6),
+  600: const Color.fromRGBO(136, 14, 79, .7),
+  700: const Color.fromRGBO(136, 14, 79, .8),
+  800: const Color.fromRGBO(136, 14, 79, .9),
+  900: const Color.fromRGBO(136, 14, 79, 1),
+};
